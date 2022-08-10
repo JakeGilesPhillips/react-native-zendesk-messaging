620c43ddb9baf917ce93127ecb0e0f8a065ce744
droplessJake <jake.gilesphillips@dropless.co.uk>
2022-08-10 19:33:49 +0100

    Revert "renamed the map"
    This reverts commit b15a7c5c87ed63a5076daa1e268085ca4411f8b2.

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare module '@dropless/react-native-zendesk-messaging' {
+  /**
+   * Initialises the Zendesk SDK 
+   * @param channelKey The channel key configured in Zendesk Admin Centre
+   */
+  export function initialise(channelKey: string): void;
+
+  /**
+   * Shows the Zendesk Messaging window
+   */
+  export function showMessaging(): void;
+}
